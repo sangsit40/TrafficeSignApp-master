21fc1c5a79011b586f78ee47e6e16cbfe53ecc8a
sangsit <boomloveevena@gmail.com>
2018-04-04 10:18:49 +0700

    Initial commit

---
+++ app/src/main/java/sdu/cs58/nawapatsaphon/trafficesignapp/DetailActivity.java
@@ -0,0 +1,31 @@
+package sdu.cs58.nawapatsaphon.trafficesignapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+    //Explicit
+    TextView titleTextView, detailTextView;
+    ImageView signImageView;
+    Button backButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        //ผูกตัวแปล
+        titleTextView = findViewById(R.id.txvTital);
+        detailTextView = findViewById(R.id.txvDetail);
+        signImageView = findViewById(R.id.imvSign);
+
+        //รับข้อมูล intent ไปแสดงผลบนหน่า XML
+        titleTextView.setText(getIntent().getStringExtra("Title"));
+        detailTextView.setText(getIntent().getStringExtra("Detail"));
+        signImageView.setImageResource(getIntent().getIntExtra("Image",R.drawable.traffic_01));
+
+    }//end onCreate Method
+}//end class
